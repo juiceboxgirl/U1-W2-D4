86950bba0a978ad9e854929632c6458ec4ddcb74
Alessia Cesaroni <alessia.cesaroni1210@gmail.com>
2023-07-06 16:14:50 +0200

    edits

--- D4/D4.js
+++ D4/D4.js
@@ -30,11 +30,11 @@ console.log("------esercizio 2------");
 
 function crazySum(n1, n2) {
   if (n1 === n2) {
-    result = Math.floor((n1 + n2) * 3);
-    console.log("Oops! Hai inserito due numeri uguali. Il tuo risultato x3 è:", result);
+    result2 = Math.floor((n1 + n2) * 3);
+    console.log("Oops! Hai inserito due numeri uguali. Il tuo risultato x3 è:", result2);
   } else {
-    let result = Math.floor(n1 + n2);
-    console.log("Il tuo risultato è:", result);
+    let result2 = Math.floor(n1 + n2);
+    console.log("Il tuo risultato è:", result2);
   }
 }
 
@@ -52,11 +52,11 @@ m2 = 19;
 
 function crazyDiff(m1, m2) {
   if (m1 > 19) {
-    let result = Math.abs((m1 - m2) * 3);
-    console.log("Oops! Uno dei due valori è > 19. Il tuo risultato x3 è:", result);
+    let result3 = Math.abs((m1 - m2) * 3);
+    console.log("Oops! Uno dei due valori è > 19. Il tuo risultato x3 è:", result3);
   } else {
-    let result = Math.abs(m1 - m2);
-    console.log("La tua differenza assoluta è:", result);
+    let result3 = Math.abs(m1 - m2);
+    console.log("La tua differenza assoluta è:", result3);
   }
 }
 
@@ -64,10 +64,25 @@ crazyDiff(8, m2);
 crazyDiff(45, m2);
 
 /* ESERCIZIO 4
- Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure se non è uguale a 400.
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure se n è uguale a 400.
 */
 
 console.log("------esercizio 4------");
+
+function boundary(n) {
+  if (n === 20 || n <= 100 || n === 400) {
+    let result4 = true;
+    console.log("Congrats! Your function is:", result4);
+  } else {
+    let result4 = false;
+    console.log("Oh no! Something went wrong. Your function is:", result4);
+  }
+}
+
+boundary(20);
+boundary(76);
+boundary(400);
+boundary(399);
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -77,6 +92,23 @@ console.log("------esercizio 4------");
 
 console.log("------esercizio 5------");
 
+str = "";
+str2 = str.split(" ");
+str3 = str2[0];
+
+function epify(str) {
+  if (str3 === "EPICODE") {
+    let result5 = str;
+    console.log(result5);
+  } else if (str3 !== "EPICODE") {
+    let result5 = "EPICODE" + " " + str;
+    console.log(result5);
+  }
+}
+
+epify("EPICODE is amazing");
+epify("is awesome");
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
@@ -84,11 +116,36 @@ console.log("------esercizio 5------");
 
 console.log("------esercizio 6------");
 
+function check3and7(n4) {
+  if (Math.abs(n4 % 3 === 0)) {
+    console.log("Il numero è multiplo di 3");
+  } else if (Math.abs(n4 % 7 === 0)) {
+    console.log("Il numero è multiplo di 7");
+  } else {
+    console.log("Il numero non è multiplo di 3 né di 7");
+  }
+}
+
+check3and7(9);
+check3and7(14);
+check3and7(34);
+
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 console.log("------esercizio 7------");
+
+function reverseString(paraStr) {
+  let splitStr = paraStr.split("");
+  let revArray = splitStr.reverse();
+  let joinArr = revArray.join("");
+  console.log(joinArr);
+}
+
+reverseString("Ciao");
+reverseString("murder");
+reverseString("Itopinonavevanonipoti");
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -96,6 +153,16 @@ console.log("------esercizio 7------");
 */
 
 console.log("------esercizio 8------");
+
+const string = "che la forza sia con te";
+
+function upperFirst(string) {
+  let splitString = string.split("");
+  let spliceString = splitString.splice(3, 1);
+  console.log(spliceString);
+}
+
+upperFirst(string);
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
@@ -107,5 +174,3 @@ console.log("------esercizio 9------");
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
-
-console.log("------esercizio 10------");
